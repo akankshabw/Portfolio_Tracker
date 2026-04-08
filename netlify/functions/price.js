82f54a47d07321147f403719503fa67257cd1ccf
Akanksha Wattamwar <akankshawattamwar@Akankshas-MacBook-Air.local>
2026-04-08 23:21:39 +0530

    setup for netlify

---
+++ netlify/functions/price.js
@@ -0,0 +1,20 @@
+exports.handler = async function (event) {
+  const symbol = event.queryStringParameters.symbol;
+
+  try {
+    const res = await fetch(
+      `https://query1.finance.yahoo.com/v8/finance/chart/${symbol}`
+    );
+    const data = await res.json();
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(data),
+    };
+  } catch (e) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: "fetch failed" }),
+    };
+  }
+};
